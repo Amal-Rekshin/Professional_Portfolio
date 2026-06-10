de5fcb5fd538923193449429a7cf5b5a8bf649cf
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2026-06-10 17:37:01 +0530

    Fix file path for resume download

--- script5.js
+++ script5.js
@@ -1,6 +1,6 @@
 function downloadResume() {
     const link = document.createElement("a");
-    link.href = ",/ANTONY AMAL REKSHIN Java Development.pdf"; // Update with your file path
+    link.href = "./Antony Amal Rekshin.pdf"; // Update with your file path
     link.download = "Antony Amal Rekshin A.pdf";
     link.click();
     alert("Downloading Resume...");
